12246df672360c067b30d90af53d64e017f64481
ALBERTO HERNANDEZ <albertoha2000@gmail.com>
2021-06-17 00:35:20 -0600

    primera fase

---
+++ src/com/company/ingredients.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.io.Serializable;
+
+public class ingredients implements Serializable {
+    private String name,units;
+    private int  quantity;
+
+    public ingredients(String name, int quantity, String units) {
+        this.name = name;
+        this.quantity = quantity;
+        this.units = units;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity () {
+        return quantity;
+    }
+
+    public String getUnits()
+    {
+        return units;
+    }
+
+}
