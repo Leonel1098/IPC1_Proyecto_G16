f84855121dd932fd0e2291db32bb9ceaff72b963
Leonel <gonzalezleonel1098@gmail.com>
2021-06-16 23:53:07 -0600

    Primera Carga

---
+++ src/com/company/config.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.io.Serializable;
+
+public class config implements Serializable {
+    private String name,address,load;
+    private int phone;
+
+    public config(String name, String address, int phone, String load)
+    {
+        this.name = name;
+        this.address=address;
+        this.phone=phone;
+        this.load=load;
+    }
+    public String getname()
+    {
+        return name;
+    }
+    public String getaddress()
+    {
+        return address;
+    }
+    public int getPhone(){return phone;};
+    public String getLoad(){return load;}
+}
