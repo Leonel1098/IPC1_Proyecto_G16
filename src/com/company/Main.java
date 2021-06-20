c75f703accd473b7f161a78beaaed1f095cd9900
Leonel <gonzalezleonel1098@gmail.com>
2021-06-19 18:39:59 -0600

    Casi Terminado 2.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-//
+
 public class Main implements Serializable {
     public static Gson gson = new Gson();
     public static Scanner u = new Scanner(System.in);
